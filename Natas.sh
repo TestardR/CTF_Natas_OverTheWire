0104627f3ef23f3188e37245c3904980262a1579
Romain <rmtestard@gmail.com>
2019-01-29 21:46:12 +0100

    lvl1

---
+++ Natas.sh
@@ -0,0 +1,13 @@
+lvl 0 : 
+http://natas0.natas.labs.overthewire.org/
+natas0
+natas0
+// inspect source code
+gtVrDuiDfck831PqWsLEZy5gyDz1clto 
+
+lvl 0 --> lvl 1
+http://natas1.natas.labs.overthewire.org/
+natas1
+gtVrDuiDfck831PqWsLEZy5gyDz1clto
+// ctrl maj j
+    // In elements go for <div id="content">
