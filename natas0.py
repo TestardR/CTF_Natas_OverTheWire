ba8cc75e0fbd08a419107f1e3d3f6b453763a960
Romain <rmtestard@gmail.com>
2019-01-30 13:27:26 +0100

    python function

---
+++ natas0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import requests
+import re
+
+username = 'natas0'
+password = username
+
+url = 'http://%s.natas.labs.overthewire.org' % username
+
+response = requests.get(url, auth=(username, password))
+print(response.text)
