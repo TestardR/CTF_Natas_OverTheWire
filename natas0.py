e550411d05de1cb96e76a9d3031d1d782f73ab7d
Romain <rmtestard@gmail.com>
2019-01-30 13:32:23 +0100

    python function to get password

--- natas0.py
+++ natas0.py
@@ -9,4 +9,6 @@ password = username
 url = 'http://%s.natas.labs.overthewire.org' % username
 
 response = requests.get(url, auth=(username, password))
-print(response.text)
+content = response.text
+
+print(re.findall('<!--The password for natas1 is (.*) -->', content))
