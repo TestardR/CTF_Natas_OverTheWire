131b0577151b3eea21e8f3ab952a5b25363589cf
Romain <rmtestard@gmail.com>
2019-01-30 13:45:33 +0100

    python func for natas2

--- Natas.sh
+++ Natas.sh
@@ -5,7 +5,7 @@ natas0
 // inspect source code
 gtVrDuiDfck831PqWsLEZy5gyDz1clto 
 
-lvl 0 --> lvl 1
+lvl 1 --> lvl 2
 http://natas1.natas.labs.overthewire.org/
 natas1
 gtVrDuiDfck831PqWsLEZy5gyDz1clto
@@ -14,10 +14,11 @@ gtVrDuiDfck831PqWsLEZy5gyDz1clto
 <!--The password for natas2 is ZluruAthQk7Q2MqmDeTiUij2ZvWy2mBi -->
 
 
-lvl 1 --> lvl 2
+lvl 2 --> lvl 3
 http://natas2.natas.labs.overthewire.org/
 natas2
 ZluruAthQk7Q2MqmDeTiUij2ZvWy2mBi
-// ctrl maj j
-    // In elements go for <div id="wechallform" style="display: block;" class="ui-draggable"><p>Submit token</p><form id="realwechallform" action="https://www.wechall.net/10-levels-on-Natas.html" enctype="application/x-www-form-urlencoded" method="post"><input type="hidden" name="wfid" value="3"><input type="hidden" name="password_solution" value="ZluruAthQk7Q2MqmDeTiUij2ZvWy2mBi"><input type="hidden" name="igotitnow" value="Register"></form></div>
-
+ctrl u
+atas2.natas.labs.overthewire.org/files
+http://natas2.natas.labs.overthewire.org/files/users.txt
+sJIJNW6ucpu6HPZ1ZAchaDtwd7oGrD14
