c016b51fe0e4978451350881a50442705772cd79
Romain <rmtestard@gmail.com>
2019-01-30 13:13:36 +0100

    lvl 2

--- Natas.sh
+++ Natas.sh
@@ -11,3 +11,13 @@ natas1
 gtVrDuiDfck831PqWsLEZy5gyDz1clto
 // ctrl maj j
     // In elements go for <div id="content">
+<!--The password for natas2 is ZluruAthQk7Q2MqmDeTiUij2ZvWy2mBi -->
+
+
+lvl 1 --> lvl 2
+http://natas2.natas.labs.overthewire.org/
+natas2
+ZluruAthQk7Q2MqmDeTiUij2ZvWy2mBi
+// ctrl maj j
+    // In elements go for <div id="wechallform" style="display: block;" class="ui-draggable"><p>Submit token</p><form id="realwechallform" action="https://www.wechall.net/10-levels-on-Natas.html" enctype="application/x-www-form-urlencoded" method="post"><input type="hidden" name="wfid" value="3"><input type="hidden" name="password_solution" value="ZluruAthQk7Q2MqmDeTiUij2ZvWy2mBi"><input type="hidden" name="igotitnow" value="Register"></form></div>
+
