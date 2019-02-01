1d522cfc9a4b8bc4bc4808b213088ca3cab7181e
Romain <rmtestard@gmail.com>
2019-02-01 09:45:35 +0100

    PHP Base64 & He

--- natas8.py
+++ natas8.py
@@ -9,8 +9,11 @@ password = 'DBfUBfqQG69KvJvJ1iAbMoIpwSNQ9bWe'
 url = 'http://%s.natas.labs.overthewire.org/' % username
 
 # response = requests.get(url, auth=(username, password))
-response = requests.post(url, data = {'secret': 'oubWYf2kBq', 'submit': 'submit'}, auth=(username, password))
+response = requests.post(
+    url, data={'secret': 'oubWYf2kBq', 'submit': 'submit'}, auth=(username, password))
 content = response.text
 
 print(content)
 print(re.findall('The password for natas9 is (.*)', content))
+
+# PHP Base64 & Hex
