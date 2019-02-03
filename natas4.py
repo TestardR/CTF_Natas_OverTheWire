03413853809b230cfca59e79615b389e72cee376
Romain <rmtestard@gmail.com>
2019-02-03 19:48:19 +0100

    HTTP headers

--- natas4.py
+++ natas4.py
@@ -8,12 +8,15 @@ password = 'Z9tkRkWmpt9Qr7XrR5jWRkgOU901swEZ'
 
 headers = {"Referer": "http://natas5.natas.labs.overthewire.org/"}
 
-url = 'http://%s.natas.labs.overthewire.org/' % username
+url = f'http://{username}.natas.labs.overthewire.org/'
 
+# response = requests.get(url, auth=(username, password))
 response = requests.get(url, auth=(username, password), headers=headers)
 content = response.text
+output = open('output.html', 'w')
+output.write(content)
 
 print(content)
-print(re.findall('The password for natas5 is(.*)', content))
+# print(re.findall('The password for natas5 is(.*)', content))
 
 # HTTP Headers & Cookies
