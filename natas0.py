dab5effc843bfbda3857f447aee85b8beeedf760
Romain <rmtestard@gmail.com>
2019-02-03 19:30:04 +0100

    view source

--- natas0.py
+++ natas0.py
@@ -6,9 +6,11 @@ import re
 username = 'natas0'
 password = username
 
-url = 'http://%s.natas.labs.overthewire.org' % username
+url = f'http://{username}.natas.labs.overthewire.org'
 
 response = requests.get(url, auth=(username, password))
 content = response.text
+output = open('output.html', 'w')
+output.write(content)
 
 print(re.findall('<!--The password for natas1 is (.*) -->', content))
