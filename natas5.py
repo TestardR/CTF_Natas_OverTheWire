cbcfae105388e2891ef852103647acbf3bc7b022
Romain <rmtestard@gmail.com>
2019-02-03 19:52:37 +0100

    cookies

--- natas5.py
+++ natas5.py
@@ -6,14 +6,17 @@ import re
 username = 'natas5'
 password = 'iX6IOfmpN7AYOQGPwtn3fXpbaJVJcHfq'
 
-url = 'http://%s.natas.labs.overthewire.org/' % username
-cookies = { 'loggedin': '1'}
+url = f'http://{username}.natas.labs.overthewire.org/'
+cookies = {'loggedin': '1'}
 
 session = requests.Session()
-response = session.get(url, auth=(username, password), cookies = cookies)
+response = session.get(url, auth=(username, password), cookies=cookies)
+# response = session.get(url, auth=(username, password))
 content = response.text
+output = open('output.html', 'w')
+output.write(content)
 
 print(content)
-print(session.cookies['loggedin'])
+# print(session.cookies['loggedin'])
 
 # HTTP Headers & Cookies
