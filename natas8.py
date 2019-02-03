90d80697fd0fc0c9975cb5c659362f3a037522b5
Romain <rmtestard@gmail.com>
2019-02-03 20:32:38 +0100

    PHP Base64 & Hex

--- natas8.py
+++ natas8.py
@@ -6,14 +6,16 @@ import re
 username = 'natas8'
 password = 'DBfUBfqQG69KvJvJ1iAbMoIpwSNQ9bWe'
 
-url = 'http://%s.natas.labs.overthewire.org/' % username
+url = f'http://{username}.natas.labs.overthewire.org/'
 
-# response = requests.get(url, auth=(username, password))
+# response = requests.get(url + 'index-source.html', auth=(username, password))
 response = requests.post(
     url, data={'secret': 'oubWYf2kBq', 'submit': 'submit'}, auth=(username, password))
 content = response.text
+output = open('output.html', 'w')
+output.write(content)
 
 print(content)
-print(re.findall('The password for natas9 is (.*)', content))
+# print(re.findall('The password for natas9 is (.*)', content))
 
 # PHP Base64 & Hex.
